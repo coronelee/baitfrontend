fd96cda7419ebdb502210d3762914cc6a2614714
coronelee <79922854+coronelee@users.noreply.github.com>
2024-03-07 09:25:36 +0300

    add

--- src/components/WorksComponent.vue
+++ src/components/WorksComponent.vue
@@ -20,9 +20,11 @@ onMounted(() => {
   <div
     class="text-white w-5/6 h-dvh flex-col font-maintext text-xl flex justify-center items-center animate-[openPage_0.5s_ease-in-out]"
   >
-    <span>Вот наши выполненные проекты</span>
+    <span class="text-2xl">Вот наши выполненные проекты</span>
     <div class="w-full h-auto flex justify-start items-center overflow-y-hidden" id="slider">
-      <div class="flex [&>div]:w-[200px] gap-4 [&>div]:h-[400px] [&>div]:bg-slate-300">
+      <div
+        class="flex [&>div]:w-[600px] max-[1200px]:[&>div]:w-[400px] max-[650px]:[&>div]:w-[200px] gap-4 [&>div]:h-[400px] [&>div]:bg-slate-300"
+      >
         <div v-for="i in 5" :key="i" class="flex justify-center items-center text-black">
           {{ i }}
         </div>
